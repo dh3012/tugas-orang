592057e124b439ffc2f8f75a04bfb6e75e0652de
dh3012 <77946863+dh3012@users.noreply.github.com>
2021-03-24 11:29:36 +0700

    Update main.java

--- main.java
+++ main.java
@@ -3,12 +3,10 @@ public class Main
     public static void main(String[] args)
     {
      int a = 5;
-      for(int j = 0 ;j <= 4; j++){
+      for(int j = 0 ;j <= 2; j++){
          for(int k = 0; k <= 4; k++){
            System.out.print(" * ");
-           if(j > 2){
-             return;
-           }
+           
            
            if(j + k == 4){
             
